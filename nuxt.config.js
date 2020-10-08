6c0a3000ec9cda2547698459b80ec3b7607c3b6a
Marvin Marinas <mlmarinas@gmail.com>
2020-10-08 12:10:29 -0600

    changed port and host

--- nuxt.config.js
+++ nuxt.config.js
@@ -109,7 +109,7 @@ export default {
   },
 
   server: {
-    port: 3000, // default: 3000
-    host: 'localhost' // default: localhost
+    port: 80, // default: 3000
+    host: '0.0.0.0' // default: localhost
   }
 };
